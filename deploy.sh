9efe3ef5636689aca095559977caa5d80f73fcbd
Timothy Sutton <tim@synthist.net>
2017-03-07 11:37:47 -0500

    Deploy as macops-ca-deploy SSH user

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,15 @@
 bundle exec jekyll build --trace
 
 # rsync params:
+# - specify a private key
 # - recursive
 # - timestamps
 # - compressed
 # - prefer checksums to compare sync rather than timestamps
 # - delete items on destination that don't exist on source
 rsync -rtvz \
+    -e 'ssh -i ~/.ssh/id_rsa_macops_ca_deploy' \
     --checksum \
     --delete \
     _site/ \
-    root@macops.ca:/var/www/macops.ca/
+    macops-ca-deploy@macops.ca:/var/www/macops.ca/
