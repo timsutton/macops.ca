c8a90ad3784a81aee580ffc09e94c2bbe9b3400e
Timothy Sutton <tim@synthist.net>
2017-07-03 11:10:28 -0400

    Switch to trusty beta, use default Python

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 HUGO_VERSION=0.24.1
 PYGMENTS_VERSION=2.2.0
 
-wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb
+wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb
 sudo dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
 
 pip install pygments==${PYGMENTS_VERSION}
