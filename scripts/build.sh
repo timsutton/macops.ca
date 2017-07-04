2172e43b33c375753aad5fe7280ce5b029082fb6
Timothy Sutton <tim@synthist.net>
2017-07-04 10:27:03 -0400

    Try sudo-less travis build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -e
 
-hugo -v
+./hugo -v
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,8 @@
 HUGO_VERSION=0.24.1
 PYGMENTS_VERSION=2.2.0
 
-wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb
-sudo dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
-
+curl -Lsf \
+    -o hugo.tar.gz \
+    https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo.tar.gz
 pip install pygments==${PYGMENTS_VERSION}
