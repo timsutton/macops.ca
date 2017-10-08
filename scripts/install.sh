5e01b1d3887d8bb96f2ab9fb716ac677d1af200b
Timothy Sutton <tim@synthist.net>
2017-10-08 13:49:15 +0200

    Replace pygments with chroma, borland theme
    - upgrade to Hugo 0.29
    - remove python lang and pip caching from travis

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,8 @@
 #!/bin/sh -e
 
-HUGO_VERSION=0.25.1
-PYGMENTS_VERSION=2.2.0
+HUGO_VERSION=0.29
 
 curl -Lsf \
     -o hugo.tar.gz \
     https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar -xzf hugo.tar.gz
-pip install pygments==${PYGMENTS_VERSION}
