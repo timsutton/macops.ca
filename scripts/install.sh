3a5dc61fa9602ab99a9d1814cf8b9870ba930f61
Timothy Sutton <tim@synthist.net>
2017-10-23 09:54:56 -0400

    install.sh supports both Linux/macOS

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,16 @@
 
 HUGO_VERSION=0.30.2
 
+platform=Linux
+if [ $(uname) = "Darwin" ]; then
+	platform=macOS
+fi
+
+tmpfile=$(mktemp)
 curl -Lsf \
-    -o hugo.tar.gz \
-    https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo.tar.gz
+    -o "${tmpfile}" \
+    https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_${platform}-64bit.tar.gz
+tar -xzf \
+	"${tmpfile}" \
+	hugo
+rm "${tmpfile}"
